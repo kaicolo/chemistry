9ad56a928a76ed7fe2e514674fc9df277882a79c
kaicolo <michaeljonlight@gmail.com>
2024-06-17 11:38:16 +1200

    Add files via upload

---
+++ 1.4.2_Periodic_Table_of_Elements/javascript/nav_menuON_OFF.js
@@ -0,0 +1,49 @@
+$('#alkali_metals').click(function() {
+    $(this).toggleClass('alkaliOFF');
+    $(this).toggleClass('alkaliON');
+});
+
+$('#alkaline_earth').click(function() {
+    $(this).toggleClass('alkaline_earthOFF');
+    $(this).toggleClass('alkaline_earthON');
+});
+
+$('#transitions').click(function() {
+    $(this).toggleClass('transitionsOFF');
+    $(this).toggleClass('transitionsON');
+});
+
+$('#lanthanides').click(function() {
+    $(this).toggleClass('lanthanidesOFF');
+    $(this).toggleClass('lanthanidesON');
+});
+
+$('#actinides').click(function() {
+    $(this).toggleClass('actinidesOFF');
+    $(this).toggleClass('actinidesON');
+});
+
+$('#metals').click(function() {
+    $(this).toggleClass('metalsOFF');
+    $(this).toggleClass('metalsON');
+});
+
+$('#metalloids').click(function() {
+    $(this).toggleClass('metalloidsOFF');
+    $(this).toggleClass('metalloidsON');
+});
+
+$('#xmetals').click(function() {
+    $(this).toggleClass('non_metalsOFF');
+    $(this).toggleClass('non_metalsON');
+});
+
+$('#halogens').click(function() {
+    $(this).toggleClass('halogensOFF');
+    $(this).toggleClass('halogensON');
+});
+
+$('#noble_gases').click(function() {
+    $(this).toggleClass('noble_gasesOFF');
+    $(this).toggleClass('noble_gasesON');
+});// JavaScript Document
---
+++ 1.4.2_Periodic_Table_of_Elements/javascript/zintl_line.js
@@ -0,0 +1,196 @@
+$(document).ready(function(){
+  $("#buttonShow").click(function() {				
+	$(".border").hide();
+    $("#step1").show();
+	$("#step1").animate({height:"50px"},500,
+
+function() {
+      $("#step2").show();
+      $("#step2").animate({width:"50px"},500,
+	   
+function() {
+        $("#step3").show();
+        $("#step3").animate({height:"50px"},500,
+		
+function() {
+          $("#step4").show();
+          $("#step4").animate({width: "50px"},500,
+		  
+function() {
+          $("#step5").show();
+          $("#step5").animate({height: "50px"},500,
+		  
+		  function() {
+        $("#step6").show();
+        $("#step6").animate({width:"50px"},500,
+		
+function() {
+          $("#step7").show();
+          $("#step7").animate({height: "50px"},500,
+		  
+function() {
+          $("#step8").show();
+          $("#step8").animate({width: "50px"},500,
+		  
+function() {
+          $("#step9").show();
+          $("#step9").animate({height: "50px"},500,
+		  
+		  function() {
+        $("#step10").show();
+        $("#step10").animate({width:"50px"},500,
+		
+function() {
+          $("#step11").show();
+          $("#step11").animate({height: "50px"},500,
+		  
+		   function() {
+            $("#buttonHide").fadeIn(250);
+          });
+        });
+      });
+    });
+  });
+});
+});
+});
+});
+});
+});
+});
+});
+
+
+
+$(document).ready(function(){
+  $("#buttonHide").click(function() {				
+	$(".border").hide();
+	$("#step1").show();
+	$("#step2").show();
+	$("#step3").show();
+	$("#step4").show();
+	$("#step5").show();
+	$("#step6").show();
+	$("#step7").show();
+	$("#step8").show();
+	$("#step9").show();
+	$("#step10").show();
+    $("#step11").show();	
+	$("#step11").animate({height:"0px"},500,
+	
+function() {
+	$(".border").hide();
+	$("#step1").show();
+	$("#step2").show();
+	$("#step3").show();
+	$("#step4").show();
+	$("#step5").show();
+	$("#step6").show();
+	$("#step7").show();
+	$("#step8").show();
+	$("#step9").show();
+      $("#step10").show();
+      $("#step10").animate({width:"0px"},500,
+	 
+
+	   
+function() {
+		$(".border").hide();
+		$("#step1").show();
+		$("#step2").show();
+		$("#step3").show();
+		$("#step4").show();
+		$("#step5").show();
+		$("#step6").show();
+		$("#step7").show();
+		$("#step8").show();
+        $("#step9").show();
+        $("#step9").animate({height:"0px"},500,
+
+		
+function() {
+		$(".border").hide();
+		$("#step1").show();
+		$("#step2").show();
+		$("#step3").show();
+		$("#step4").show();
+		$("#step5").show();
+		$("#step6").show();
+		$("#step7").show();
+        $("#step8").show();
+        $("#step8").animate({width: "0px"},500,
+
+		  
+function() {
+		$(".border").hide();
+		$("#step1").show();
+		$("#step2").show();
+		$("#step3").show();
+		$("#step4").show();
+		$("#step5").show();
+		$("#step6").show();
+        $("#step7").show();
+        $("#step7").animate({height: "0px"},500,
+
+		  
+function() {
+		$(".border").hide();
+		$("#step1").show();
+		$("#step2").show();
+		$("#step3").show();
+		$("#step4").show();
+		$("#step5").show();	  
+        $("#step6").show();
+        $("#step6").animate({width:"0px"},500,
+		
+function() {
+        $(".border").hide();
+		$("#step1").show();
+		$("#step2").show();
+		$("#step3").show();
+		$("#step4").show();
+		$("#step5").show();
+        $("#step5").animate({height: "0px"},500,
+		  
+function() {
+	    $(".border").hide();
+		$("#step1").show();
+		$("#step2").show();
+		$("#step3").show();
+        $("#step4").show();
+        $("#step4").animate({width: "0px"},500,
+		  
+function() {
+        $(".border").hide();
+		$("#step1").show();
+		$("#step2").show();
+		$("#step3").show();
+        $("#step3").animate({height: "0px"},500,
+		  
+		  function() {
+        $(".border").hide();
+		$("#step1").show();
+		$("#step2").show();
+        $("#step2").animate({width:"0px"},500,
+		
+function() {
+          $(".border").hide();
+		  $("#step1").show();
+          $("#step1").animate({height: "0px"},500,
+		  
+		   function() {
+			$(".border").hide();
+            $("#buttonShow").fadeIn(250);
+          });
+        });
+      });
+    });
+  });
+});
+});
+});
+});
+});
+});
+});
+});
